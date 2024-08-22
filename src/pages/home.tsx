4ca04f866b550511436ccbc50131e5b6886419e4
ivan110501 <ivan.antunovic292@gmail.com>
2024-08-22 14:58:08 +0200

    update

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,9 +6,6 @@ import Tag from "../components/tag";
 import croatiaFlagUrl from "../assets/croatia.png";
 import IconArrowTopRight from "../assets/arrow-top-right";
 
-import { useState, useEffect } from "react";
-import Select from "react-select";
-
 type ProfileDataType = {
   firstName: string;
   lastName: string;
@@ -31,8 +28,7 @@ const data1: ProfileDataType = {
   profileDescription: "I'm a frontend developer based in Osijek.",
   skills: ["HTML", "CSS/SASS", "JS/TS", "React", "Fusion360"],
   experience:
-    "I'm a frontend developer and 3D designer passionate about experiences for web applications. I also enjoy doing 3D design and 3D printing. This site is an example of web app made by me. Bellow is demonstration of API use. Click on Pokemon name to show how it looks like :) ",
-
+    "I'm a frontend developer and 3D designer passionate about experiences for web applications. I also enjoy doing 3D design and 3D printing. This site is an example of a web app made by me from scratch. Some of my 3D projects are listed down below ",
   links: {
     github: "https://github.com/ivan110501",
     instagram: "https://www.instagram.com/ivan.antunovic3/",
@@ -41,76 +37,7 @@ const data1: ProfileDataType = {
   email: "ivan.antunovic292@gmail.com",
 };
 
-type PokemonsResponseType = {
-  count: number;
-  next: string;
-  results: PokemonType[];
-};
-
-type PokemonType = {
-  name: string;
-  url: string;
-};
-
-type ActivePokemonType = {
-  sprites: {
-    front_default: string;
-  };
-};
-
-//ItemPerPage
-type OptionType = {
-  value: string;
-  label: string;
-};
-
-const options: OptionType[] = [
-  { value: "1", label: "1 Pokemon" },
-  { value: "2", label: "2 Pokemons" },
-  { value: "3", label: "3 Pokemons" },
-];
-
-const defaultLimit = "3";
-
 const Home = () => {
-  const [data, setData] = useState<PokemonType[]>([]);
-  const [limit, setLimit] = useState<string | undefined>("2");
-
-  const [activePokemon, setActivePokemon] = useState<
-    ActivePokemonType | undefined
-  >(undefined);
-
-  const getData = async (limit: string | undefined) => {
-    await fetch(
-      `https://pokeapi.co/api/v2/pokemon/?limit=${limit ? limit : defaultLimit}`
-    )
-      .then((data) => {
-        return data.json();
-      })
-      .then((res: PokemonsResponseType) => {
-        setData(res.results);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  const getPokemonData = async (pokemon: string) => {
-    await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-      .then((data) => {
-        return data.json();
-      })
-      .then((res: ActivePokemonType) => {
-        setActivePokemon(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  useEffect(() => {
-    getData(limit);
-  }, [limit]);
   return (
     <>
       <div className="personal">
@@ -141,36 +68,26 @@ const Home = () => {
           <div className="personal__body__pannel personal__body__main">
             <h2 className="personal__body__title">About me</h2>
             <p className="personal__body__p mb-8">{data1.experience}</p>
-            <Select
-              onChange={(e) => {
-                //getData(e?.value);
-                setLimit(e?.value);
-              }}
-              options={options}
-            />
+
             <div>
-              {data.map((pokemon) => {
-                return (
-                  <button
-                    className="pokebutton"
-                    onClick={(e) => {
-                      const test = e.target as HTMLButtonElement;
-                      getPokemonData(test.innerHTML);
-                    }}
-                    key={pokemon.name}
-                  >
-                    {pokemon.name}
-                  </button>
-                );
-              })}
+              <h3>Projects</h3>
+
+              <div style={{ display: "flex", marginTop: "10%" }}>
+                <img
+                  style={{ width: "50%" }}
+                  src="https://www.upwork.com/att/download/portfolio/persons/uid/1652076715133968384/profile/projects/files/a89524f9-7e32-43d9-a7ee-7f6cdbaacced"
+                  alt=""
+                />
+                <img
+                  style={{ width: "50%" }}
+                  src="https://www.upwork.com/att/download/portfolio/persons/uid/1652076715133968384/profile/projects/files/d5ae96b6-1a9a-4814-9092-4fbf55142b8a"
+                  alt=""
+                />
+              </div>
             </div>
 
             <div style={{ height: "5rem" }}>
-              <img
-                style={{ width: "8rem" }}
-                src={activePokemon?.sprites.front_default}
-                alt=""
-              />
+              <img style={{ width: "8rem" }} alt="" />
             </div>
           </div>
           <div className="personal__body__pannel personal__body__side">
